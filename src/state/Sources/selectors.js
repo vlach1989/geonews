12f1023ec2ed164ad913800f3148b129d4cb7b9d
pvlach <vlach1989@gmail.com>
2018-10-28 20:34:18 +0100

    Loading status

--- src/state/Sources/selectors.js
+++ src/state/Sources/selectors.js
@@ -28,8 +28,42 @@ const getAllByType = createSelector(
     }
 );
 
+const getByKey = createSelector(
+    [getAllByKey, (state, key) => key],
+    (models, key) => {
+        return models ? models[key] : null;
+    }
+);
+
+const getGroupedLoadingStatus = createSelector(
+    [getAll],
+    (models) => {
+        let status = {
+            total: 0,
+            started: 0,
+            success: 0,
+            error: 0
+        };
+        if (models){
+            models.forEach(model => {
+                status.total++;
+                if (model.loadingStatus && model.loadingStatus === "started"){
+                    status.started++;
+                } else if (model.loadingStatus && model.loadingStatus === "success"){
+                    status.success++;
+                } else if (model.loadingStatus && model.loadingStatus === "error"){
+                    status.error++;
+                }
+            });
+        }
+        return status;
+    }
+);
+
 export default {
     getAll,
     getAllByKey,
-    getAllByType
+    getAllByType,
+    getByKey,
+    getGroupedLoadingStatus
 };
